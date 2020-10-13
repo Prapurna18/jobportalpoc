4daf5e9e00ffaf86ebd1b7effbf37f864748ba2a
Prapurna Devi <deviprapurna18@gmail.com>
2020-10-13 03:15:17 -0700

    created list of jobs dynamically using javascript

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,37 @@
 
 import React from 'react';
+
 function Home(){
+    let jobList = ["job list 1", "job list 2", "job list 3", "job list 4","job list 5"];
+
+    const getListOfJobs = ()=>{
+        for(let i=0;i<jobList.length;i++){
+        jobList[i]=<li>{jobList[i]}</li>
+        }
+        return jobList;
+    }
+    const jobportal = ()=>{
+        return <h1>WELCOME TO JOB PORTAL</h1>;
+
+
+    
+    }
+    
+    
     return(
         <div>
-                    
-            
+            {
+                jobportal()
+            }
             
             <ul>
-                <li>job list 1</li>
-                <li>job list 2</li>
-                <li>job list 3</li>
+                {
+                   getListOfJobs() 
+                }
+               
             </ul>
         </div>
     )
 }
+
         export default Home;
